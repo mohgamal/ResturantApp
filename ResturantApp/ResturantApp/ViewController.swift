37e90b44b2694b3dfce2df219329ca68f90120f9
Mohammed Abdelaty <dev.mohgamal@gmail.com>
2021-01-20 08:47:16 +0200

    adding main app project and domain framework

---
+++ ResturantApp/ResturantApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ResturantApp
+//
+//  Created by Mohammed Abd El-Aty on 20/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ResturantAppDomain/ResturantAppDomain/ResturantAppDomain.h
@@ -0,0 +1,18 @@
+//
+//  ResturantAppDomain.h
+//  ResturantAppDomain
+//
+//  Created by Mohammed Abd El-Aty on 20/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ResturantAppDomain.
+FOUNDATION_EXPORT double ResturantAppDomainVersionNumber;
+
+//! Project version string for ResturantAppDomain.
+FOUNDATION_EXPORT const unsigned char ResturantAppDomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResturantAppDomain/PublicHeader.h>
+
+
