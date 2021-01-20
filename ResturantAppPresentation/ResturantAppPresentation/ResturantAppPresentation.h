5134d6c641476cf2ee232c33d4c798547ffbc8b7
Mohammed Abdelaty <dev.mohgamal@gmail.com>
2021-01-20 08:49:15 +0200

    adding presentation and data layer frameworks

---
+++ ResturantAppPresentation/ResturantAppPresentation/ResturantAppPresentation.h
@@ -0,0 +1,18 @@
+//
+//  ResturantAppPresentation.h
+//  ResturantAppPresentation
+//
+//  Created by Mohammed Abd El-Aty on 20/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ResturantAppPresentation.
+FOUNDATION_EXPORT double ResturantAppPresentationVersionNumber;
+
+//! Project version string for ResturantAppPresentation.
+FOUNDATION_EXPORT const unsigned char ResturantAppPresentationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResturantAppPresentation/PublicHeader.h>
+
+
---
+++ ResturnatAppData/ResturnatAppData/ResturnatAppData.h
@@ -0,0 +1,18 @@
+//
+//  ResturnatAppData.h
+//  ResturnatAppData
+//
+//  Created by Mohammed Abd El-Aty on 20/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ResturnatAppData.
+FOUNDATION_EXPORT double ResturnatAppDataVersionNumber;
+
+//! Project version string for ResturnatAppData.
+FOUNDATION_EXPORT const unsigned char ResturnatAppDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResturnatAppData/PublicHeader.h>
+
+
