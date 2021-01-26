0bc2c73d23648ab2b44dfb6812cd82e58abae2bf
Mohammed Abdelaty <dev.mohgamal@gmail.com>
2021-01-26 14:35:20 +0200

    update CategoriesResultModel to match result API field names

--- ResturnatAppData/ResturnatAppData/Categories/CategoriesModel.swift
+++ ResturnatAppData/ResturnatAppData/Categories/CategoriesModel.swift
@@ -9,29 +9,29 @@ import Foundation
 import ResturantAppDomain
 
 public struct CategoriesResultModel: Codable {
-    public let categories: [CategoryModel]?
+    public let data: [CategoryModel]?
     public let links: LinksModel?
     public let meta: MetaDataModel?
 
     public struct CategoryModel: Codable {
         public let id: UUID?
         public let name: String?
-        public let nameLocalized: String?
+        public let name_localized: String?
         public let refrence: String?
         public let image: String?
-        public let createdAt: String?
-        public let updatedAt: String?
-        public let deletedAt: String?
+        public let created_at: String?
+        public let updated_at: String?
+        public let deleted_at: String?
         
         func dotCategoriesEntity() -> CategoriesResultEntity.CategoryEntity {
-            return CategoriesResultEntity.CategoryEntity(id: id, name: name, nameLocalized: nameLocalized, refrence: refrence, image: image, createdAt: createdAt, updatedAt: updatedAt, deletedAt: deletedAt)
+            return CategoriesResultEntity.CategoryEntity(id: id, name: name, nameLocalized: name_localized, refrence: refrence, image: image, createdAt: created_at, updatedAt: updated_at, deletedAt: deleted_at)
         }
     }
     
     func getCategoriesList() -> [CategoriesResultEntity.CategoryEntity] {
         var categoriesResult: [CategoriesResultEntity.CategoryEntity] = []
         
-        for category in categories ?? [] {
+        for category in data ?? [] {
             categoriesResult.append(category.dotCategoriesEntity())
         }
         
