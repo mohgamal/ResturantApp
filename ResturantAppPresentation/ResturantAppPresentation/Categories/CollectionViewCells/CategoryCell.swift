27f0751ee16de76d59115c0b335e4b93fb2df80a
Mohammed Abdelaty <dev.mohgamal@gmail.com>
2021-01-27 09:49:11 +0200

    adding category collection view cell

---
+++ ResturantAppPresentation/ResturantAppPresentation/Categories/CollectionViewCells/CategoryCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCell.swift
+//  ResturantAppPresentation
+//
+//  Created by Mohammed Abd El-Aty on 26/01/2021.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
